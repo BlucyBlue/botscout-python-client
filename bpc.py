b490fd46af2a9c5b475184129e61fa13315b2805
BlucyBlue <rastko.pavlovic@protonmail.com>
2020-03-15 09:29:04 +0100

    Function for reading query dictionaries from a csv file.

--- bpc.py
+++ bpc.py
@@ -7,6 +7,21 @@ from query import make_query, validate_qparams
 module_name = "Botscout Python Client: unofficial Python client for Botscout.com API."
 __version__ = "0.0.1"
 
+ACCEPTABLE_SEARCH_PARAMS = ['multi', 'mail', 'ip', 'name', 'all']
+
+def params_from_csv(filename):
+	params = []
+	with open(filename, "r") as f:
+		reader = csv.reader(f, delimiter=",")
+		next(reader, None)
+		for row in reader:
+			params.append({'mail':row[0], 
+						   'ip':row[1],
+						   'name': row[2],
+						   'all': row[3]})
+
+	return params
+
 
 if __name__ == "__main__":
     main()
